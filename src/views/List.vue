e064c5b60b66c2425eb54b5e421886231a8fc648
chenwei <980684242@qq.com>
2018-12-10 10:53:32 +0800

    修改一下下

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="list">
-		
+		<ul>
+			<li v-for="(item,index) in product" v-text="item.title" ></li>
+		</ul>
 		<bottom></bottom>
 	</div>
 	
@@ -8,11 +10,24 @@
 
 <script>
 	import Bottom from "@/components/Footer"
+	import axios from "axios"
 	export default {
+		data(){
+			return{
+				product:[],
+			}
+		},
 		name: 'list',
 		components: {
 			Bottom
+		},
+		mounted(){
+			axios.get("./data.json").then(res=>{
+				console.log(res);
+				this.product=res.data
+			}).catch(err=>{})
 		}
+		
 	}
 </script>
 
